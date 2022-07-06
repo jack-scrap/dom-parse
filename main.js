a07797af1b4be062d35ad65b4fa3f3f2d8b5a90b
jackhasakeyboard <jackhasakeyboard@gmail.com>
2022-07-05 21:01:09 -0600

    Abstracted tokenization

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
 const ws = ' ';
 
+function tok(serial) {
+	let cont = [];
+	let i = 0;
+	while (i < serial.length) {
+		let c = serial[i];
+
+		if (c == ws) {
+			i++;
+
+			continue;
+		}
+
+		if (c != ws) {
+			let tok = "";
+			while (c != ws && i < serial.length) {
+				c = serial[i];
+
+				tok += c;
+
+				i++;
+			}
+
+			cont.push(tok);
+		}
+	}
+
+	$('#tok .body').empty();
+
+	for (let tok of cont) {
+		$('#tok .body').append(`<div>${tok}</div>`);
+	}
+
+	return cont;
+}
+
 $(document).ready(function() {
 	$('#expr').keyup(function() {
 		const expr = $('#expr').val();
 
-		let cont = [];
-		let i = 0;
-		while (i < expr.length) {
-			let c = expr[i];
-
-			if (c == ws) {
-				i++;
-
-				continue;
-			}
-
-			if (c != ws) {
-				let tok = "";
-				while (c != ws && i < expr.length) {
-					c = expr[i];
-
-					tok += c;
-
-					i++;
-				}
-
-				cont.push(tok);
-			}
-		}
-
-		$('#tok .body').empty();
-
-		for (let tok of cont) {
-			$('#tok .body').append(`<div>${tok}</div>`);
-		}
+		let serial = tok(expr);
 	});
 });
